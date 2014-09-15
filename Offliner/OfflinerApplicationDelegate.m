7853b0d07f146a9bd804f7cc7ca2831218168052
quellish <github@quellish.neomailbox.com>
2014-09-14 23:07:56 -0700

    Added memory warning handler

--- Offliner/OfflinerApplicationDelegate.m
+++ Offliner/OfflinerApplicationDelegate.m
@@ -10,6 +10,10 @@
 
 @implementation OfflinerApplicationDelegate
 
+#ifndef KBYTE
+#define KBYTE(x)   ((x) << 10)
+#endif
+
 #ifndef MBYTE
 #define MBYTE(x)   ((x) << 20)
 #endif
@@ -24,6 +28,15 @@
     return YES;
 }
 
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
+	[self resizeURLCacheMemory:[NSURLCache sharedURLCache]];
+}
+
+// UIApplicationDidEnterBackgroundNotification
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+
+}
+
 #pragma mark URL Cache
 
 - (NSURLCache *)newURLCache {
@@ -32,6 +45,8 @@
     // Configure the URL cache with 1mb of in memory cache, 10mb of on disk cache, and in our custom directory
     // If you do not do this, the default URL cache will not have on-disk storage.
     result = [[NSURLCache alloc] initWithMemoryCapacity:MBYTE(1) diskCapacity:MBYTE(10) diskPath:[self URLCachePath]];
+    // See radar 12712196
+    [result currentDiskUsage];
     
     return result;
 }
@@ -39,6 +54,14 @@
 - (NSString *) URLCachePath {
     // Foundation will put this in Library/Caches/bundle ID/ by default.
 	return @"URLCache";
+}
+
+- (void) resizeURLCacheMemory:(NSURLCache *)cache {
+    NSUInteger  originialMemoryCapacity = 0;
+    
+    originialMemoryCapacity = [cache memoryCapacity];
+    [cache setMemoryCapacity:(originialMemoryCapacity / 2)];
+    
 }
 
 #pragma mark Error handling placeholder
